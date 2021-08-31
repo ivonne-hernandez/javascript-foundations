7ed023dee9673ee70d32ded175f505e2c5adabee
Ivonne Hernandez <ivonne702@gmail.com>
2021-08-31 08:31:57 -0600

    Complete the functions for the Pirate class

---
+++ mythical-creatures/exercises/pirate.js
@@ -0,0 +1,37 @@
+class Pirate {
+
+  constructor(name, job) {
+    this.name = name;
+
+    if(job === undefined) {
+      this.job = `scallywag`;
+    } else {
+      this.job = job;
+    }
+
+    this.cursed = false;
+    this.booty = 0;
+  }
+
+  robShip() {
+    if (this.booty === 500) {
+      this.cursed = true;
+      return `ARG! I\'ve been cursed!`;
+    } else {
+      this.booty += 100;
+      return `YAARRR!`;
+    }
+  }
+
+  liftCurse() {
+    if (this.booty >= 300 && this.cursed === true) {
+      this.cursed = false;
+      this.booty -= 300;
+      return `Your curse has been lifted!`;
+    } else if (this.cursed === false) {
+      return `You don\'t need to lift a curse!`;
+    }
+  }
+}
+
+module.exports = Pirate;
