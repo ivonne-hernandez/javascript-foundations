4c23cf4e4385002136cb9a7a262326daf8bc32ac
Ivonne Hernandez <ivonne702@gmail.com>
2021-09-06 09:08:19 -0600

    Complete the functions for the medusa file

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -1,2 +1,30 @@
 var Person = require('./person');
 var Statue = require('./statue');
+
+class Medusa {
+  constructor(name) {
+    this.name = name;
+    this.statues = [];
+  }
+
+  gazeAtVictim(person) {
+    var statue = new Statue(person.name);
+
+    if(this.statues.length < 3) {
+      this.statues.push(statue);
+    } else {
+      var freedStatue = this.statues[0];      this.statues.shift();
+
+      var backToPerson = new Person(freedStatue.name);
+      backToPerson.mood = `relieved`;
+
+      this.statues.push(statue);
+      return backToPerson;
+    }
+  }
+
+
+
+
+}
+module.exports = Medusa;
