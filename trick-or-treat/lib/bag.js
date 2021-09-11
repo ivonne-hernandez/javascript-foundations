d5e8d87c8ebf521b8f579812119897a7eafb681c
Ivonne Hernandez <ivonne702@gmail.com>
2021-09-11 11:15:14 -0600

    Complete the functions for the Bag class

---
+++ trick-or-treat/lib/bag.js
@@ -0,0 +1,23 @@
+class Bag {
+  constructor() {
+    this.empty = true;
+    this.count = 0
+    this.candies = [];
+  }
+
+  fill(candy) {
+    this.candies.push(candy);
+    this.count += 1;
+  }
+
+  contains(candyType) {
+    if(this.candies[0].type === candyType) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+}
+
+module.exports = Bag;
