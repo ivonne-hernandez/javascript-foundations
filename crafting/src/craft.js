7e7910713daea209ccfff06c87e1b6c1754e1f20
Ivonne Hernandez <ivonne702@gmail.com>
2021-09-12 10:31:31 -0600

    Complete the methods for the Craft class

--- crafting/src/craft.js
+++ crafting/src/craft.js
@@ -1,4 +1,19 @@
 class Craft {
+  constructor(data){
+    this.name = data.type;
+    this.materials = data.materials;
+    this.completed = false;
+  }
+
+  completeCraft() {
+    this.completed = true;
+    return `All done! It looks great!`;
+  }
+
+  calculateProjectTotal() {
+    return this.materials[0].price * this.materials[0].amount + this.materials[1].price * this.materials[1].amount;
+  }
+
 
 }
 
