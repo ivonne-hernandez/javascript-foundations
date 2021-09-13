aea282bf2ecf4d2c081e4d14a3ecc603377eb057
Ivonne Hernandez <ivonne702@gmail.com>
2021-09-13 08:30:06 -0600

    Complete the methods for the Snack class

--- vending-machine/src/snack.js
+++ vending-machine/src/snack.js
@@ -1,5 +1,22 @@
 class Snack {
+  constructor(name, price) {
+    this.name = name;
+    this.price = price
+    this.itemsInStock = 0;
+  }
 
+  stockItems(addToStock) {
+    this.itemsInStock += addToStock;
+  }
+
+  removeItem() {
+    if(this.itemsInStock) {
+      this.itemsInStock -= 1;
+    return `Item taken! There are now ${this.itemsInStock} items left.`
+  } else {
+    return `Sorry, no ${this.name} left in stock!`;
+  }
+  }
 }
 
 module.exports = Snack;
