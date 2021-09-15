9825c5269ffff44a8839d4b81d7da1ae76ec115d
Ivonne Hernandez <ivonne702@gmail.com>
2021-09-15 08:34:52 -0600

    Complete the methods for the Vending Machine class

--- vending-machine/src/vending-machine.js
+++ vending-machine/src/vending-machine.js
@@ -1,4 +1,18 @@
 class VendingMachine {
+  constructor(data) {
+    this.id = data.id;
+    this.isBroken = data.isBroken;
+    this.snacks = [];
+  }
+
+  addSnacks(candyBar) {
+    if(!this.snacks.includes(candyBar.name)) {
+      this.snacks.push(candyBar);
+    } else {
+      return `Sorry, that snack is already stocked! Try adding a different snack.`;
+    }
+
+  }
 
 }
 
