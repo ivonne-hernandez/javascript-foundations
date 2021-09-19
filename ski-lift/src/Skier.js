38ea592206925f7a1328660170f3fad511d55467
Ivonne Hernandez <ivonne702@gmail.com>
2021-09-19 10:56:09 -0600

    Complete the methods for the Skier class

--- ski-lift/src/Skier.js
+++ ski-lift/src/Skier.js
@@ -1,4 +1,22 @@
 class Skier {
+    constructor(name, hasLiftTicket) {
+        this.name = name;
+        this.hasLiftTicket = hasLiftTicket;
+        this.skillLevel = 1;
+        this.nextSlope = `green circle`;
+    }
+
+    takeLesson() {
+        this.skillLevel += 1;
+    }
+
+    pickSlope() {
+        if (this.skillLevel === 3) {
+            this.nextSlope = `blue square`;
+        } else if (this.skillLevel >= 4) {
+            this.nextSlope = `black diamond`;
+        }
+    }
 
 }
 
