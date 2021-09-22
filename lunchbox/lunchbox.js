ea276ef5e7e468560b16ccfbdfb391d9877f347b
Ivonne Hernandez <ivonne702@gmail.com>
2021-09-22 17:55:39 -0600

    Complete the methods for the LunchBox and Snack classes

--- lunchbox/lunchbox.js
+++ lunchbox/lunchbox.js
@@ -1,4 +1,29 @@
 class LunchBox {
+  constructor(data) {
+    this.owner = data.owner;
+    this.material = data.madeOf;
+    this.shape = data.shape;
+    this.color = data.color;
+    this.snacks = [];
+    this.isInLunchBox = false;
+  }
+
+  acquireSnack(snack) {
+    this.snacks.push(snack);
+    snack.isInLunchBox = true;
+  }
+
+  findHealthySnacks() {
+    var healthySnacks = [];
+    for (var i = 0; i < this.snacks.length; i++) {
+      if (this.snacks[i].type.toLowerCase().includes(`fruit`)) {
+        healthySnacks.push(this.snacks[i].type);
+      }
+    }
+    return healthySnacks;
+  }
+
+
 
 }
 
